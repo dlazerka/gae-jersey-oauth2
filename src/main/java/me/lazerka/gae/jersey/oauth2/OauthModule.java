0b04c974be8d129bed931bcc395476bd55aa2989
Dzmitry Lazerka <dlazerka@gmail.com>
2016-01-19 16:16:29 -0800

    Added OauthModule constructors.

--- src/main/java/me/lazerka/gae/jersey/oauth2/OauthModule.java
+++ src/main/java/me/lazerka/gae/jersey/oauth2/OauthModule.java
@@ -18,26 +18,21 @@ package me.lazerka.gae.jersey.oauth2;
 
 import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier.Builder;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.io.Files;
 import com.google.inject.AbstractModule;
-import com.google.inject.Key;
-import com.google.inject.Provides;
-import com.google.inject.name.Names;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.inject.Singleton;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Set;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
@@ -48,70 +43,92 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 public class OauthModule extends AbstractModule {
 	private static final Logger logger = LoggerFactory.getLogger(OauthModule.class);
 
-	public static final String OAUTH_CLIENT_ID = "oauth.client.id";
-	public static final Set<String> ALLOWED_ISSUERS = ImmutableSet.of(
+	private static final Set<String> ALLOWED_ISSUERS = ImmutableSet.of(
 			"accounts.google.com",
 			"https://accounts.google.com");
 
-	@Override
-	protected void configure() {
-		// Read config files early, so that errors would pop up on startup.
-		bind(Key.get(String.class, Names.named(OAUTH_CLIENT_ID)))
-				.toInstance(readOauthClientId());
+	/** Default location of Client ID */
+	private static final String CLIENT_ID_FILE_PATH = "WEB-INF/keys/oauth.client_id.key";
 
-		// Choose between verification methods.
-		bind(TokenVerifier.class).to(TokenVerifierSignature.class);
-		//bind(AuthFilter.class).to(TokenVerifierRemote.class);
+	private String clientId;
+	private Method method;
+
+	public enum Method {
+		/** Verifies token signature using Google public key. */
+		SIGNATURE,
+		/** Verifies token signature by making HTTPS call to Google servers. */
+		REMOTE
 	}
 
-	@Inject
-	@Provides
-	@Singleton
-	GoogleIdTokenVerifier createTokenVerifier(
-			@Named(OAUTH_CLIENT_ID) String oauthClientId,
-			JsonFactory jsonFactory
-	) {
+	/** Reads Client ID from file {@link #CLIENT_ID_FILE_PATH}. */
+	public OauthModule() {
+		this(new File(CLIENT_ID_FILE_PATH));
+	}
+
+	/** Reads Client ID from file {@link #CLIENT_ID_FILE_PATH}. */
+	public OauthModule(File file) {
+		this(readClientIdFile(file), Method.SIGNATURE);
+	}
+
+	/**
+	 * @param clientId Issued by authorization server.
+	 */
+	public OauthModule(String clientId, Method method) {
+		checkArgument(clientId.endsWith(".apps.googleusercontent.com"), "Must end with '.apps.googleusercontent.com'");
+		this.clientId = checkNotNull(clientId);
+		this.method = checkNotNull(method);
+	}
+
+	@Override
+	protected void configure() {
+		if (method == Method.SIGNATURE) {
+
+			GoogleIdTokenVerifier tokenVerifier = createTokenVerifier(clientId, JacksonFactory.getDefaultInstance());
+			bind(GoogleIdTokenVerifier.class).toInstance(tokenVerifier);
+
+			bind(TokenVerifier.class).to(TokenVerifierSignature.class);
+
+		} else if (method == Method.REMOTE) {
+
+			bind(String.class)
+					.annotatedWith(OauthClientId.class)
+					.toInstance(clientId);
+
+			bind(JsonFactory.class).toInstance(JacksonFactory.getDefaultInstance());
+			bind(TokenVerifier.class).to(TokenVerifierRemote.class);
+
+		} else {
+			throw new IllegalArgumentException(method.toString());
+		}
+	}
+
+	private GoogleIdTokenVerifier createTokenVerifier(String oauthClientId, JsonFactory jsonFactory) {
 		logger.trace("Creating " + GoogleIdTokenVerifier.class.getSimpleName());
 		UrlFetchTransport transport = new UrlFetchTransport.Builder()
 				.validateCertificate()
 				.build();
-		return new Builder(transport, jsonFactory)
+		return new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
 				.setAudience(ImmutableSet.of(oauthClientId))
 				.setIssuers(ALLOWED_ISSUERS)
 				.build();
 	}
 
-	@Provides
-	@Singleton
-	JsonFactory getJsonFactory() {
-		return JacksonFactory.getDefaultInstance();
-	}
-
-	String readOauthClientId() {
-		File file = new File("WEB-INF/keys/oauth.client_id.key");
-		String notFoundMsg = "Put there OAuth2.0 Client ID obtained as described here " +
-				"https://developers.google.com/identity/sign-in/android/";
-
-		String result = readKeyFile(file, notFoundMsg);
-
-		if (!result.endsWith(".apps.googleusercontent.com")) {
-			throw new RuntimeException("Must end with '.apps.googleusercontent.com'");
-		}
-
-		return result;
-	}
-
 	/**
 	 * Reads whole file as a string.
 	 */
-	static String readKeyFile(File file, String notFoundMsg) {
+	static String readClientIdFile(File file) {
 		logger.trace("Reading {}", file.getAbsolutePath());
+
+		String notFoundMsg = "Put there OAuth2.0 Client ID obtained as described here " +
+				"https://developers.google.com/identity/sign-in/android/";
+
 		try {
 			String result = Files.toString(file, UTF_8)
 					.trim();
 			if (result.isEmpty()) {
 				throw new RuntimeException("File is empty: " + file.getAbsolutePath() + " " + notFoundMsg);
 			}
+
 			return result;
 		} catch (FileNotFoundException e) {
 			throw new RuntimeException("File " + file.getAbsolutePath() + " not found. " + notFoundMsg);
